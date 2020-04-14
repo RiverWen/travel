df87f1313c4f14249a2564b9157b5d23b06d874e
RiverWen <869824309@qq.com>
2020-04-14 09:48:04 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FastClick from 'fastclick'
+import './assets/reset.css'
+import './assets/border.css'
 
 Vue.config.productionTip = false
+FastClick.attach(document.body)
 
 new Vue({
   router,
